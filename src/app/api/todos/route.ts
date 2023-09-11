ea4b9e6dee32c74c86beb184de72a70e1baf2f02
Aghyad <zenlogie@gmail.com>
2023-09-11 23:52:20 +0200

    initial commit

---
+++ src/app/api/todos/route.ts
@@ -0,0 +1,23 @@
+import connectMongoDB from "@/libs/mongodb";
+import Todo from "@/models/TodoModel";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  const { title, description } = await request.json();
+  await connectMongoDB();
+  await Todo.create({ title, description });
+  return NextResponse.json({ message: "Todo Created" }, { status: 201 });
+}
+
+export async function GET() {
+  await connectMongoDB();
+  const todos = await Todo.find();
+  return NextResponse.json({ todos });
+}
+
+export async function DELETE(request: NextRequest) {
+  const id = request.nextUrl.searchParams.get("id");
+  await connectMongoDB();
+  await Todo.findByIdAndDelete(id);
+  return NextResponse.json({ message: "Todo deleted" }, { status: 200 });
+}
